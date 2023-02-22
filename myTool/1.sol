485f5ef50333922545a7048ad9cfe14c7e3702f6
Catxiaobai <2378960008@qq.com>
2023-02-22 10:34:17 +0800

    完善myTool

---
+++ myTool/1.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract Test2{
+function bug_intou_inter(Test1 t1, uint b) public view {
+    uint goal_ = t1.getGoal();
+    if(3000 < goal_) {
+        b = b + goal_;  // overflow bug inter contract
+    }
+}
+}
+
+contract Test1{
+    uint public goal = 5000;
+    function getGoal() public view returns(uint){
+        return goal;
+    }
+}
