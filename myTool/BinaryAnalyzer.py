05b998156c2f2a6cd7f8a4049bd8fd95aac6a1b7
Catxiaobai <2378960008@qq.com>
2023-02-24 09:01:50 +0800

    时间戳漏洞标红

--- myTool/BinaryAnalyzer.py
+++ myTool/BinaryAnalyzer.py
@@ -305,8 +305,10 @@ class BinaryAnalyzer:
             value.infoPrint()
             if value.function != 'NULL':
                 graph.node(str(value.startBlockPos), color='blue')
-            elif 'TIMESTAMP' in value.conditionalJumpExpression:
+            if 'TIMESTAMP' in value.instrString:
                 graph.node(str(value.startBlockPos), color='red')
+            if 'TIMESTAMP' in value.instrString:
+                print('有时间戳漏洞')
             else:
                 graph.node(str(value.startBlockPos))
             if value.fallPos != -1:
